9274f5d13468dd386b86f44eebb9bfb72f67ceed
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-04-29 15:23:47 +1000

    Wrapper shell script for cron.

---
+++ runcron.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd /home/carlo/CharacterisationVL_EC2_credentials;
+source openrc.sh
+source ec2rc.sh
+cd - &> /dev/null # Why does this print /home/carlo?
+
+/home/carlo/.cabal/bin/nova-backup-util --run-backups
+
+
