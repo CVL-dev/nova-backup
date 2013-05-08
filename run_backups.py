e42ac7da9d545b413a5f6dee0e30e65e39045b14
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-05-08 14:36:13 +1000

    Use a specific work queue, to avoid clashes with the user management celery worker.

--- run_backups.py
+++ run_backups.py
@@ -2,7 +2,5 @@ from novabackup import BackupVM
 from novabackup import nova_client
 
 for vm in nova_client().servers.list():
-    job = BackupVM.delay(vm.name)
+    job = BackupVM.apply_async(args=[vm.name], kwargs={}, queue='novabackup')
     print vm.name, job
-
-
