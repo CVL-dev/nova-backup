b092e3e66646dbce49da4756b8d37499fe6c2124
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-05-15 06:04:20 +1000

    Utilities for displaying active tasks and killing all tasks.

---
+++ print_active_jobs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+from celery import Celery
+from celery.result import AsyncResult
+
+celery = Celery('novabackup', backend='redis://localhost', broker='redis://localhost')
+
+from celery.task.control import inspect
+
+i = inspect()
+
+for d in [i.scheduled(), i.active()]:
+    for host, workers in d.iteritems():
+        for w in workers:
+            res = AsyncResult(w['id'])
+
+            print res.id
+            print res.result
+            print res.status
+            print
+
+
