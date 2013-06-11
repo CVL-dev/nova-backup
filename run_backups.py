f470cafc2343ba65d43731b22eaa890848250fbb
Carlo Hamalainen <carlo.hamalainen@gmail.com>
2013-06-12 08:57:03 +1000

    Do not run if current time between DAY_START and DAY_END.

--- run_backups.py
+++ run_backups.py
@@ -1,7 +1,15 @@
 from novabackup import BackupVM
 from novabackup import nova_client
+import sys
+import time
+
+DAY_START = 4
+DAY_END   = 10
+
+if time.localtime().tm_hour in range(DAY_START, DAY_END + 1):
+    print 'Error: not starting backups, current time is between %.2d and %.2d hours' % (DAY_START, DAY_END,)
+    sys.exit(1)
 
 for vm in nova_client().servers.list():
-    # job = BackupVM.apply_async(args=[vm.name], kwargs={}, queue='novabackup')
-    # print job, vm.name
-    print vm.name
+    job = BackupVM.apply_async(args=[vm.name], kwargs={}, queue='novabackup')
+    print job, vm.name
